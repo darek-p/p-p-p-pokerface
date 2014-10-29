d63cfa9bdd08e8601f32906707dad1f0a68329b3
MM <mm@MMs-MacBook-Pro.local>
2014-10-29 10:24:49 +0100

    p-p-p-pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,50 @@
 (ns p-p-p-pokerface)
 
+(def replacement {\T 10, \J 11, \Q 12, \K 13, \A 14})
 (defn rank [card]
-  nil)
+   (let [[r _] card]
+    (cond
+       (Character/isDigit r) (Integer/valueOf (str r))
+       :else (replacement r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (< 1 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+    (< 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+      (< 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (== 1 (count (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) (range 2 4)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [m (sort (vals (frequencies (map rank hand))))]
+    (or (= m [1 2 2]) (= m [1 4]))))
 
 (defn straight? [hand]
-  nil)
+  (let [m (sort (map (fn [x] (Integer/valueOf x))
+       (keys (frequencies (replace {14 1} (map rank hand))))))
+   r (range (first m) (+ (first m) 5))] (= m r)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers? #{ [high-card? 0] [pair? 1]
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4]  [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+      ] (apply max (map second (filter (fn [x] ((first x) hand)) (seq checkers?))))))
